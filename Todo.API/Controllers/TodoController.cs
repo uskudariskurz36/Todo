488739c0d186787b49df48a790efeb0eab105c72
Murat Başeren <uskudar.iskur.z36@gmail.com>
2023-01-09 15:17:32 +0300

    .

--- Todo.API/Controllers/TodoController.cs
+++ Todo.API/Controllers/TodoController.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
 using Todo.API.Entities;
 using Todo.API.Models;
 
 namespace Todo.API.Controllers
 {
-    [Route("[controller]/[action]")]
+    [Route("[controller]")]
     [ApiController]
     public class TodoController : ControllerBase
     {
@@ -22,12 +23,54 @@ namespace Todo.API.Controllers
         }
 
 
+        [HttpGet("{count}/{order}")]
+        public IActionResult List(int count, string order)
+        {
+            List<TodoItem> items = null;
+
+            if (order == "asc")
+                items = _databaseContext.Todos.OrderBy(x => x.Id).Take(count).ToList();
+            else if (order == "desc")
+                items = _databaseContext.Todos.OrderByDescending(x => x.Id).Take(count).ToList();
+            else
+                items = _databaseContext.Todos.Take(count).ToList();
+
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            TodoItem item = _databaseContext.Todos.Find(id);
+
+            if (item == null)
+            {
+                return NotFound(id);
+            }
+
+            return Ok(item);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById2(int id)
+        {
+            TodoItem item = _databaseContext.Todos.Find(id);
+
+            if (item == null)
+            {
+                return NotFound(id);
+            }
+
+            return Ok(item);
+        }
+
+
         [HttpPost]
         public IActionResult Create(TodoCreateModel model)
         {
             if (model.Text == "test")
             {
-                return BadRequest(model);
+                return BadRequest(model);   // status code : 400
             }
 
             TodoItem item = new TodoItem();
@@ -38,7 +81,43 @@ namespace Todo.API.Controllers
             _databaseContext.Todos.Add(item);
             _databaseContext.SaveChanges();
 
-            return Created("", item);
+            return Created("", item);   // status code : 201
+        }
+
+        // PUT : /Todo/Edit/{id}
+        [HttpPut("{id}")]
+        public IActionResult Edit([FromRoute] int id, [FromBody] TodoUpdateModel model)
+        {
+            TodoItem item = _databaseContext.Todos.Find(id);
+
+            if (item == null)
+            {
+                return NotFound();  // status code : 404
+            }
+
+            item.Text = model.Text;
+            item.DueDate = model.DueDate;
+            item.Completed = model.Completed;
+
+            _databaseContext.SaveChanges();
+
+            return Ok(item);    // status code : 200
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove([FromRoute] int id)
+        {
+            TodoItem item = _databaseContext.Todos.Find(id);
+
+            if (item == null)
+            {
+                return NotFound();  // status code : 404
+            }
+
+            _databaseContext.Todos.Remove(item);
+            _databaseContext.SaveChanges();
+
+            return Ok();    // status code : 200
         }
     }
 }
--- Todo.API/Models/TodoModels.cs
+++ Todo.API/Models/TodoModels.cs
@@ -9,4 +9,13 @@ namespace Todo.API.Models
 
         public DateTime? DueDate { get; set; }
     }
+
+    public class TodoUpdateModel
+    {
+        [StringLength(500)]
+        public string Text { get; set; }
+
+        public DateTime? DueDate { get; set; }
+        public bool Completed { get; set; }
+    }
 }
