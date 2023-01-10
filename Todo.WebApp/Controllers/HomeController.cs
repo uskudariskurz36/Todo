eeeb1471d0555d458691212ace7daff49e16ac5a
Murat Başeren <uskudar.iskur.z36@gmail.com>
2023-01-10 14:04:51 +0300

    .

--- Todo.WebApp/Controllers/HomeController.cs
+++ Todo.WebApp/Controllers/HomeController.cs
@@ -2,20 +2,90 @@
 using RestSharp;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
 using Todo.WebApp.Models;
 
 namespace Todo.WebApp.Controllers
 {
     public class HomeController : Controller
     {
+        RestClient client = new RestClient("http://localhost:5086");
+
         public IActionResult Index()
         {
-            RestClient client = new RestClient("http://localhost:5086");
             RestRequest request = new RestRequest("/Todo/List", Method.Get);
 
             List<Models.Todo> todos = client.Get<List<Models.Todo>>(request);
 
             return View(todos);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TodoCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+                RestRequest request = new RestRequest("/Todo/Create", Method.Post);
+                request.AddJsonBody(model);
+
+                RestResponse<Models.Todo> response = client.ExecutePost<Models.Todo>(request);
+                //Models.Todo todo = client.Post<Models.Todo>(request);
+
+                Models.Todo todo = response.Data;
+
+                if (response.StatusCode != HttpStatusCode.Created)
+                {
+                    ModelState.AddModelError("", "Servis erişim hatası.");
+                    //ModelState.AddModelError("", response.ErrorMessage);
+
+                    return View(model);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            RestRequest request = new RestRequest($"/Todo/GetById/{id}", Method.Get);
+            RestResponse<Models.Todo> response = client.ExecuteGet<Models.Todo>(request);
+
+            if (response.IsSuccessful == false)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(response.Data);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Models.Todo model)
+        {
+            if (ModelState.IsValid)
+            {
+                RestRequest request = new RestRequest($"/Todo/Edit/{id}", Method.Put);
+                request.AddJsonBody(model);
+
+                RestResponse<Models.Todo> response = client.ExecutePut<Models.Todo>(request);
+
+                if (response.IsSuccessful)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Güncelleme yapılamadı(servis hatası).");
+                }
+            }
+
+            return View(model);
         }
 
         public IActionResult Privacy()
--- Todo.WebApp/Models/Todo.cs
+++ Todo.WebApp/Models/Todo.cs
@@ -1,4 +1,6 @@
-﻿namespace Todo.WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Todo.WebApp.Models
 {
     public class Todo
     {
@@ -6,5 +8,14 @@
         public string Text { get; set; }
         public DateTime? DueDate { get; set; }
         public bool Completed { get; set; }
+    }
+
+    public class TodoCreate
+    {
+        [Required]
+        [StringLength(500)]
+        public string Text { get; set; }
+
+        public DateTime? DueDate { get; set; }
     }
 }
